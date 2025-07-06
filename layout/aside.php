bd826ed000da41aed59f48696ba4e4d824b77f10
José Rangel <jose.rangel7030@gmail.com>
2025-07-05 22:39:20 -0600

    Test Adds

--- layout/aside.php
+++ layout/aside.php
@@ -1,3 +1,7 @@
 <aside>
     <h3>Publicidad</h3>
+    <div class="adds-container">
+        <img src="img/adds/add1.jpg" width="100%" height="auto">
+        <img src="img/adds/add2.jpg" width="100%" height="auto">
+    </div>
 </aside>
