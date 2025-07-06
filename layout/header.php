db716552814c7846da226e8002978793a94662fd
José Rangel <jose.rangel7030@gmail.com>
2025-07-05 21:23:58 -0600

    Web Site Icon

--- layout/header.php
+++ layout/header.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/estilos.css">
     <title>Conoce Tampico</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon.png">
 </head>
+
 <body>
     <div class="contenedor">
         <header>
